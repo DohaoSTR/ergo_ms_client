eb6e8327879426a69c74e18d0d849fd918ab74fa
kotomafia <retrosmith2@gmail.com>
2025-07-15 13:42:19 +0300

    feat: добавление страницы дашбордов и обновление конфигурации меню

--- src/modules/bi/components/CardSidebarBody.vue
+++ src/modules/bi/components/CardSidebarBody.vue
@@ -2,6 +2,7 @@
 
 const props = defineProps({
   icon: { type: [Object, String, Function], required: true },
+  icolor: { type: String, required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
   targetPage: { type: String, required: true }
@@ -15,12 +16,45 @@ const triggerSidebar = () => {
 </script>
 
 <template>
-  <div class="d-flex flex-column align-items-center justify-content-center text-center">
-    <component class="mb-4 text-secondary-emphasis" :is="icon" :size="35" />
-    <h5 class="mb-3">{{ title }}</h5>
-    <p class="mb-4">{{ description }}</p>
-    <button class="btn btn-primary mt-auto" @click="triggerSidebar">Открыть</button>
+  <div class="card-body">
+    <div class="card-icon">
+      <component class="mb-3 card-icon-accent" :style="{ color: icolor }" :is="icon" :size="35" />
+    </div>
+    <div class="card-title">
+      <h5 class="mb-3">{{ title }}</h5>
+    </div>
+    <div class="card-description">
+      <p class="mb-3">{{ description }}</p>
+    </div>
+    <div class="card-button">
+      <button class="btn btn-primary" @click="triggerSidebar">Открыть</button>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card-body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 12pt;
+  box-sizing: border-box;
+}
+.card-icon{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card-description {
+  flex: 1 1 auto;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100%;
+}
+.card-button {
+  display: flex;
+  justify-content: center;
+}
+</style>
